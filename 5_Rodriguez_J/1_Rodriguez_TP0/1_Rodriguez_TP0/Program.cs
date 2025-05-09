a0aace4894ae7354bde31a9b468b523a42fd7ba3
DZeugirdor <alejandrodilan43@gmail.com>
2025-05-09 17:20:00 -0300

    Add files via upload

---
+++ 5_Rodriguez_J/1_Rodriguez_TP0/1_Rodriguez_TP0/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1_Rodriguez_TP0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresá tu edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Sos mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("Sos menor de edad.");
+            }
+        }
+    }
+}
---
+++ 5_Rodriguez_J/1_Rodriguez_TP4/1_Rodriguez_4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _1_Rodriguez_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Ingresá tu edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            
+            Console.Write("Ingresá tus ingresos mensuales: ");
+            int ingresos = int.Parse(Console.ReadLine());
+
+            
+            if ((edad > 19 && ingresos <= 100000) ||
+                (edad < 19 && ingresos == 0) ||
+                (edad == 19 && ingresos <= 50000))
+            {
+                Console.WriteLine("¡Podés cobrar la beca Dilan");
+            }
+            else
+            {
+                Console.WriteLine("No cumplís con los requisitos para cobrar la beca.");
+            }
+        }
+    }
+}
