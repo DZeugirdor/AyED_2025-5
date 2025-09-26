130b6e18e1248aafa2e967fa0d51b41ea623c846
DZeugirdor <alejandrodilan43@gmail.com>
2025-09-26 14:50:21 -0300

    Add files via upload

---
+++ 5_Rodriguez_J/2_Rodriguez_tp8/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2_Rodriguez_tp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de filas (n): ");
+            int n = int.Parse(Console.ReadLine());
+            
+            Console.Write("Ingrese la cantidad de columnas (m): ");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, m];
+
+            Console.WriteLine("\nMatriz de " + n + " x " + m + " con todos los elementos en cero:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nPresione una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+ }
+
