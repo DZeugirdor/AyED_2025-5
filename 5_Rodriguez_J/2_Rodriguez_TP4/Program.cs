e8b7909a70d5f33b15dc1f8d519c3138a2ad5878
DZeugirdor <alejandrodilan43@gmail.com>
2025-09-26 14:39:57 -0300

    Add files via upload

---
+++ 5_Rodriguez_J/2_Rodriguez_TP4/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _2_Rodriguez_TP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Análisis de Precios - Hot Sale ===");
+
+            
+            Console.Write("Ingrese la cantidad de productos vendidos: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            double precio;
+            double mayor = 0;
+            double menor = 0;
+
+            
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.Write("Ingrese el precio del producto " + (i + 1) + ": ");
+                precio = double.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    
+                    mayor = precio;
+                    menor = precio;
+                }
+                else
+                {
+                    if (precio > mayor)
+                    {
+                        mayor = precio;
+                    }
+                    if (precio < menor)
+                    {
+                        menor = precio;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nProducto más caro:" + mayor);
+            Console.WriteLine("Producto más económico: " + menor);
+        }
+    }
+}
+
