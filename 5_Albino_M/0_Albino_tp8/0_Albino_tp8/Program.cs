680111b216bc54fa4cef6d4982ac9fd9ea59f638
JJonibla <johanncito2009@gmail.com>
2025-09-30 14:43:08 -0300

    Add files via upload

---
+++ 5_Albino_M/0_Albino_tp8/0_Albino_tp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Albino_tp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el primer número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            int resultado = numero1 / numero2;
+
+            Console.WriteLine("El resultado de la resta es: " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
