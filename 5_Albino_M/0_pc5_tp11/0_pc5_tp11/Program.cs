32442bb3166e7e30c525f8a68f951718d89fbd81
JJonibla <johanncito2009@gmail.com>
2025-09-30 14:41:29 -0300

    Add files via upload

---
+++ 5_Albino_M/0_pc5_tp11/0_pc5_tp11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Albino_tp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el ancho del rectángulo (x): ");
+            double ancho = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la altura del rectángulo (y): ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double perimetro = 2 * (ancho + altura);
+            double area = ancho * altura;
+            double diagonal = Math.Sqrt((ancho * ancho) + (altura * altura));
+
+            Console.WriteLine("\nResultados:");
+            Console.WriteLine($"- Perímetro: {perimetro}");
+            Console.WriteLine($"- Área: {area}");
+            Console.WriteLine($"- Diagonal: {diagonal}");
+
+            Console.WriteLine("\nPresione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
